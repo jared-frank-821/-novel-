f180cf2394dc4b887dc431f4e3c31b424df998ba
jaredfrank <2761906353@qq.com>
2026-03-09 23:25:45 +0800

    2.0解决editor编辑器无法在浏览器显示，解决tiptap和浏览器水合冲突

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+"use client"
 import Image from "next/image";
+import config from "../../tailwind.config";
+import dynamic from 'next/dynamic';
 
+const Editor=dynamic(()=>import('./compoments/Editor'),{
+  ssr:false,
+  loading:()=><p>编辑器正在加载中</p>
+})
 export default function Home() {
   return (
-    <div>第一个项目</div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-50">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm">
+        <h1 className="text-4xl font-bold mb-8 text-center text-gray-800">
+          My AI Novel Editor
+        </h1>
+        {/* 这里就是连接点：把我们写的编辑器组件放进来 */}
+        <Editor />
+      </div>
+    </main>
   );
 }
